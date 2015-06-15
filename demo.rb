798c071c24202503243a004f6e224f7b9fb52e4f
tzwm <wangjunzhe4444@gmail.com>
2015-06-15 13:37:10 +0800

    demo

--- demo.rb
+++ demo.rb
@@ -1,8 +1,4 @@
 output = <<-OUT
-Modified for demo
-Modified for demo
-Modified for demo
-Modified for demo
 Using worker: worker-linux-027f0490-1.bb.travis-ci.org:travis-linux-16
 travis_fold:start:system_info
 [0K[33;1mBuild system information[0m
